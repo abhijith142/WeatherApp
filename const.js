6c64439a835b2097fca53ef3a499c5258a4c6bdd
Abhijith <abhijith.142@gmail.com>
2017-12-29 17:24:50 +0530

    got the location from geocode

--- const.js
+++ const.js
@@ -1,3 +1,4 @@
 module.exports = {
-  API_KEY : '19090343df3841b6ba7154341172812'
+  API_KEY : '19090343df3841b6ba7154341172812',
+  Google_API_KEY : 'AIzaSyBj8_4HOCspAqfOQnEeQEpNjsyqTRMaGw8'
 }
--- public/app/app.js
+++ public/app/app.js
@@ -14,6 +14,41 @@ app.config(function($routeProvider){
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position){
             console.log(position);
+
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                geocoder = new google.maps.Geocoder();
+
+                var latlng = new google.maps.LatLng(lat, lng);
+                geocoder.geocode({'latLng': latlng}, function(results, status) {
+                  if (status == google.maps.GeocoderStatus.OK) {
+                  console.log(results)
+                    if (results[1]) {
+                     //formatted address
+                     alert(results[0].formatted_address)
+                    //find country name
+                         for (var i=0; i<results[0].address_components.length; i++) {
+                          for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+                          //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                              if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                                  //this is the object you are looking for
+                                  city= results[0].address_components[i];
+                                  break;
+                              }
+                          }
+                      }
+                    }
+                  }
+                });
+
+
+
+
+
+
+
+
           });
         }
         return weatherService.getReportBasedOnLatitude("4567","1234")
