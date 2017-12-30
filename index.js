192e4991fdc6474a21d5a2d93c8adf3ad18599a8
abhijith142 <abhijith142@gmail.com>
2017-12-30 17:35:17 +0530

    https

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const router = require("./routes/route");
+
+var sslOptions = {
+  key: fs.readFileSync('Certs/key.pem'),
+  cert: fs.readFileSync('Certs/cert.pem'),
+  passphrase:"password"
+};
 
 app.use(express.static(__dirname + "/public"));
 
@@ -9,3 +17,4 @@ app.use("/",router);
 app.listen(process.env.PORT || 9090,function(){
   console.log("server running at 9090");
 })
+https.createServer(sslOptions, app).listen(process.env.PORT || 9091)
--- public/app/service/weatherService.js
+++ public/app/service/weatherService.js
@@ -1,14 +1,14 @@
 angular.module("app").service("weatherService",function($http){
   this.getReportBasedOnLocation=function(location){
     return $http({
-        url:"http://localhost:9090/getReportBasedOnLocation",
+        url:"https://localhost:9091/getReportBasedOnLocation",
         method:"GET",
         params:{location:location}
     })
   }
   this.getReportBasedOnLatitude=function(latitude,longitude){
     return $http({
-        url:"http://localhost:9090/getReportBasedOnLatitude",
+        url:"https://localhost:9091/getReportBasedOnLatitude",
         method:"GET",
         params:{latitude:latitude,
         longitude:longitude}
