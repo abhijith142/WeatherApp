a0d12646f03c669aa1a99bea12f5c3ac8ad7fc05
abhijith142 <abhijith142@gmail.com>
2017-12-30 17:46:23 +0530

    heroku location

--- public/app/service/weatherService.js
+++ public/app/service/weatherService.js
@@ -1,14 +1,14 @@
 angular.module("app").service("weatherService",function($http){
   this.getReportBasedOnLocation=function(location){
     return $http({
-        url:"https://localhost:443/getReportBasedOnLocation",
+        url:"https://weathereporterair.herokuapp.com/getReportBasedOnLocation",
         method:"GET",
         params:{location:location}
     })
   }
   this.getReportBasedOnLatitude=function(latitude,longitude){
     return $http({
-        url:"https://localhost:443/getReportBasedOnLatitude",
+        url:"https://weathereporterair.herokuapp.com/getReportBasedOnLatitude",
         method:"GET",
         params:{latitude:latitude,
         longitude:longitude}
