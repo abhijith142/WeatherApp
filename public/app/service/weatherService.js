5bd4898a8a619f8db38e7a170fac626132eff10a
abhijith142 <abhijith142@gmail.com>
2017-12-30 17:41:30 +0530

    443

--- public/app/service/weatherService.js
+++ public/app/service/weatherService.js
@@ -1,14 +1,14 @@
 angular.module("app").service("weatherService",function($http){
   this.getReportBasedOnLocation=function(location){
     return $http({
-        url:"https://localhost:9091/getReportBasedOnLocation",
+        url:"https://localhost:443/getReportBasedOnLocation",
         method:"GET",
         params:{location:location}
     })
   }
   this.getReportBasedOnLatitude=function(latitude,longitude){
     return $http({
-        url:"https://localhost:9091/getReportBasedOnLatitude",
+        url:"https://localhost:443/getReportBasedOnLatitude",
         method:"GET",
         params:{latitude:latitude,
         longitude:longitude}
